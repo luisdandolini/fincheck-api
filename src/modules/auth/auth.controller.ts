aa7e4135713b499711019fd532a0f404f33d5317
Luis Dandolini Duarte <luisfernandodandoliniduarte@gmail.com>
2026-06-23 17:18:29 -0300

    feat: create auth guard with decorator isPublic for public routes

--- src/modules/auth/auth.controller.ts
+++ src/modules/auth/auth.controller.ts
@@ -2,18 +2,21 @@ import { Body, Controller, Post } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { SignupDto } from './dto/signup.dto';
 import { SigninDto } from './dto/signin.dto';
+import { IsPublic } from '../../shared/decorators/IsPublic';
 
 @Controller('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
+  @IsPublic()
   @Post('signin')
   signin(@Body() signinDto: SigninDto) {
     return this.authService.signin(signinDto);
   }
 
+  @IsPublic()
   @Post('signup')
-  create(@Body() signupDto: SignupDto) {
+  signup(@Body() signupDto: SignupDto) {
     return this.authService.signup(signupDto);
   }
 }
