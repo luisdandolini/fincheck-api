72cdd0f4ce5a4b911055eac9d890725a7dc4b830
Luis Dandolini Duarte <luisfernandodandoliniduarte@gmail.com>
2026-06-24 15:44:31 -0300

    feat: create module categories with findAll categories service

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,9 +5,10 @@ import { AuthModule } from './modules/auth/auth.module';
 import { APP_GUARD } from '@nestjs/core';
 import { AuthGuard } from './modules/auth/auth.guard';
 import { JwtService } from '@nestjs/jwt';
+import { CategoriesModule } from './modules/categories/categories.module';
 
 @Module({
-  imports: [UsersModule, DatabaseModule, AuthModule],
+  imports: [UsersModule, DatabaseModule, AuthModule, CategoriesModule],
   controllers: [],
   providers: [
     JwtService,
