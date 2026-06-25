cfb461d0abed33bb6de6b1b8d6704bc6b5c9e8d0
Luis Dandolini Duarte <luisfernandodandoliniduarte@gmail.com>
2026-06-25 16:23:56 -0300

    feat: create module and repository

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,9 +6,10 @@ import { APP_GUARD } from '@nestjs/core';
 import { AuthGuard } from './modules/auth/auth.guard';
 import { JwtService } from '@nestjs/jwt';
 import { CategoriesModule } from './modules/categories/categories.module';
+import { BankAccountsModule } from './modules/bank-accounts/bank-accounts.module';
 
 @Module({
-  imports: [UsersModule, DatabaseModule, AuthModule, CategoriesModule],
+  imports: [UsersModule, DatabaseModule, AuthModule, CategoriesModule, BankAccountsModule],
   controllers: [],
   providers: [
     JwtService,
