a2bb17a5baad4b8d8479265fd230e797768b13d2
Luis Dandolini Duarte <luisfernandodandoliniduarte@gmail.com>
2026-07-16 13:44:13 -0300

    feat: create get transactions with filters (month, year, bankAccountId, type)

--- src/modules/transactions/services/transactions.service.ts
+++ src/modules/transactions/services/transactions.service.ts
@@ -5,6 +5,7 @@ import { TransactionsRepository } from '../../../shared/database/repositories/tr
 import { ValidateBankAccountOwnerShipService } from '../../bank-accounts/services/validate-bank-account-ownership.service';
 import { ValidateCategoryOwnerShipService } from '../../categories/services/validate-category-ownership.service';
 import { ValidateTransactionOwnerShipService } from './validate-transaction-ownership.service';
+import { TransactionType } from '../entities/Transaction';
 
 @Injectable()
 export class TransactionsService {
@@ -38,9 +39,25 @@ export class TransactionsService {
     });
   }
 
-  findAllByUserId(userId: string) {
+  findAllByUserId(
+    userId: string,
+    filters: {
+      month: number;
+      year: number;
+      bankAccountId?: string;
+      type?: TransactionType;
+    },
+  ) {
     return this.transactionsRepo.findMany({
-      where: { userId },
+      where: {
+        userId,
+        bankAccountId: filters.bankAccountId,
+        type: filters.type,
+        date: {
+          gte: new Date(Date.UTC(filters.year, filters.month)),
+          lt: new Date(Date.UTC(filters.year, filters.month + 1)),
+        },
+      },
     });
   }
 
